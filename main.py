4ef0e979ff02b73cfe4a5ce1930687bd960f10ff
dark-hermes <hermawansent@gmail.com>
2021-04-04 20:18:31 +0700

    Fix typing and add comments

--- main.py
+++ main.py
@@ -18,21 +18,35 @@ while True:
     # 3 ~ 6 is a good value for minimum neighbors
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+    # Iterate faces for x and y coordinates
+    # w and h for weight and height
     for x, y, w, h in faces:
+
+        # Draw rectangles around detected faces
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
 
+        # Take Region of Interest of frame in gray scale and rgb
         roi_gray = gray[y:y + h, x:x + w]
-        roi_bgr = frame[y:y + h, x:x + w]
+        roi_rgb = frame[y:y + h, x:x + w]
 
+        # Detect eye in multi scale form region of interest
+        # with the same parameters as faces
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
 
+        # Iterates eyes for it's features
+        # ex and ey for each detected eyes X and Y coordinates
+        # ew and eh for each eyes weights and heights coordinates
         for ex, ey, ew, eh in eyes:
-            cv2.rectangle(roi_bgr, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 3)
+            # draw rectangles around the eyes
+            cv2.rectangle(roi_rgb, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 3)
 
+    # Show the video capture
     cv2.imshow('Frame', frame)
 
+    # Press 'q' on keyboard will close the program
     if cv2.waitKey(1) == ord('q'):
         break
 
+# Release the capture and close the windows
 cap.release()
 cv2.destroyAllWindows()
